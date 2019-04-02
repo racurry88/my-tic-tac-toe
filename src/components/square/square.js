3c7ee4e71e3761bf89827f084a0457c2d64b2b0b
Ren Curry <racurry88@gmail.com>
2019-04-01 22:15:35 -0700

    Including start of test scripts

--- src/components/square/square.js
+++ src/components/square/square.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export default function Square({ onClick, value }) {
-    return (
-      <button className="square" onClick={onClick}>
-        {value}
-      </button>
-    );
+  return (
+    <button className="square" onClick={onClick} data-testid="square">
+      {value}
+    </button>
+  );
 }
--- src/components/square/square.test.js
+++ src/components/square/square.test.js
@@ -1,1 +1,29 @@
-/* TODO: Complete some tests */
+import React from 'react';
+import chai, { expect } from 'chai';
+import chaiDom from 'chai-dom';
+import { render, fireEvent } from 'react-testing-library';
+
+import Square from './square';
+
+chai.use(chaiDom);
+
+describe('Components/Square', () => {
+  it('renders the value', () => {
+    const { getByTestId } = render((
+      <Square onClick={() => {}} value="X" />
+    ));
+
+    expect(getByTestId('square')).to.contain.text('X');
+  });
+
+  xit('calls the function when clicked', () => {
+    const onClick = jest.fn();
+    const { getByTestId } = render((
+      <Square onClick={onClick} value="X" />
+    ));
+
+    fireEvent.click(getByTestId('square'));
+
+    expect(onClick.mock.calls).to.have.length(1);
+  });
+});
