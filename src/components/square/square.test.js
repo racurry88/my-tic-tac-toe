05a477af2911619d95b354304cd9fd9ce8ce07d0
Ren Curry <racurry88@gmail.com>
2019-04-05 15:08:26 -0700

    Add test outlines

--- src/components/square/square.test.js
+++ src/components/square/square.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import chai, { expect } from 'chai';
 import chaiDom from 'chai-dom';
-import { render, fireEvent } from 'react-testing-library';
+import { render, waitForElement, fireEvent } from 'react-testing-library';
 
 import Square from './square';
 
@@ -16,13 +16,16 @@ describe('Components/Square', () => {
     expect(getByTestId('square')).to.contain.text('X');
   });
 
-  xit('calls the function when clicked', () => {
+  xit('calls the function when clicked', async () => {
     const onClick = jest.fn();
+    const buttonClick = { button: 2 }
     const { getByTestId } = render((
       <Square onClick={onClick} value="X" />
     ));
 
-    fireEvent.click(getByTestId('square'));
+    waitForElement(() => getByTestId('square'));
+
+    fireEvent.submit(getByTestId('square'),buttonClick);
 
     expect(onClick.mock.calls).to.have.length(1);
   });
