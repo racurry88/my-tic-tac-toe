d245cfae38b1cd143939b9a8b4a7147104c0a721
Christopher Baker <christopher@bitovi.com>
2019-04-05 14:56:24 -0700

    fix event test

--- src/components/square/square.test.js
+++ src/components/square/square.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import chai, { expect } from 'chai';
 import chaiDom from 'chai-dom';
-import { render, fireEvent } from 'react-testing-library';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 
 import Square from './square';
 
 chai.use(chaiDom);
+afterEach(cleanup);
 
 describe('Components/Square', () => {
   it('renders the value', () => {
@@ -16,7 +17,7 @@ describe('Components/Square', () => {
     expect(getByTestId('square')).to.contain.text('X');
   });
 
-  xit('calls the function when clicked', () => {
+  it('calls the function when clicked', () => {
     const onClick = jest.fn();
     const { getByTestId } = render((
       <Square onClick={onClick} value="X" />
